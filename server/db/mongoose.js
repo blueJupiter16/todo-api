b5f9c855c244e2d544322b7b7ca6848520a88dc1
Junaid <junaid.tinwala.96@gmail.com>
2018-06-18 12:36:19 +0530

    db connections changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var uri = 'mongodb+srv://tinwalajunaid@gmail.com:Lenovo1996!@cluster0-t2gwr.mongodb.net/test?retryWrites=true';
+var uri = 'mongodb+srv://user1:password_user1@cluster0-t2gwr.mongodb.net/test?retryWrites=true';
 mongoose.connect(`${uri}/TodoApp`);
 //mongoose.connect(`mongodb://localhost/TodoApp`);
 
