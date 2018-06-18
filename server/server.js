9fc67e2e8c849c731472cfe080dc37d529e233e3
Junaid <junaid.tinwala.96@gmail.com>
2018-06-18 11:42:27 +0530

    Atlas and Heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {User} = require('./models/user');
 const {log} = require('../helper/logger.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -40,6 +41,6 @@ app.get('/todos/:id', (req,res) => {
     });
 });
 
-app.listen(3000, () => {
-    log('Started on port 3000');
+app.listen(port, () => {
+    log(`Started on port ${port}`);
 });
