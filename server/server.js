c39a02c2b522de129ac47ae173fc558db48a1e7c
Junaid <junaid.tinwala.96@gmail.com>
2018-06-19 23:28:12 +0530

    added patch route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo');
@@ -24,26 +26,48 @@ app.post('/todos', (req,res) => {
     });
 });
 
-app.delete('todos/:id', (req,res) => {
+app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if(ObjectID.isValid(id)){
-        Todo.findByIdAndRemove(id).then((doc) => {
+  
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+  
+    Todo.findByIdAndRemove(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+  
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    });
+  });
 
-            if(!doc) {
-                res.status(404).send();
-            }
-            else{
-                res.send(doc);
-            }
-            
-        }).catch((err) => {
-            log(err);
-        });
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send();
     }
-    else{
-        res.status(404).send('Not a valid ID');
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+
+    }else{
+        body.completed = false;
+        body.completedAt = null;
     }
-    
+
+    Todo.findByIdAndUpdate(id,{$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 
 app.get('/todos', (req,res) => {
