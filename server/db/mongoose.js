f7f9db3026ad0f90786ea3f77e018609631d2ddd
Junaid <junaid.tinwala.96@gmail.com>
2018-06-19 12:08:20 +0530

    db changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,9 @@ mongoose.Promise = global.Promise;
 var port = process.env.PORT || 27017;
 var uri = 'mongodb://user1:password_user1@cluster0-shard-00-00-t2gwr.mongodb.net:27017,cluster0-shard-00-01-t2gwr.mongodb.net:27017,cluster0-shard-00-02-t2gwr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 //var uri = 'mongodb+srv://user1:password_user1@cluster0-t2gwr.mongodb.net/TodoApp?retryWrites=true';
-mongoose.connect(`${uri}`);
+var res = encodeURIComponent(uri);
+
+mongoose.connect(`${res}`);
 //mongoose.connect(`mongodb://localhost/TodoApp`);
 
 module.exports = {
