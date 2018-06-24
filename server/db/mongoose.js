c6828c287d64e8cd89fee45d39d8b695022e0356
Junaid <junaid.tinwala.96@gmail.com>
2018-06-24 21:39:34 +0530

    added login route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,12 @@ mongoose.Promise = global.Promise;
 var uri = 'mongodb://user1:zigmEnQn4NqIHKbV@cluster0-shard-00-00-t2gwr.mongodb.net:27017,cluster0-shard-00-01-t2gwr.mongodb.net:27017,cluster0-shard-00-02-t2gwr.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
 mongoose.connect(`${uri}`/*,{autoIndex: false}*/ ,(err) => {
-    console.log(`Error: ${err}`);
+    if(err) {
+        console.log(`Error: ${err}`);
+    } else {
+        console.log('Connected to DB');
+    }
+
 });
 //mongoose.connect(`mongodb://localhost/TodoApp`);
 
--- server/server.js
+++ server/server.js
@@ -105,9 +105,19 @@ app.post('/users', (req,res) => {
     });
 });
 
-//private route
+//private route - to authenticate API request
 app.get('/users/me',authenticate, (req,res) => {
     res.send(req.user);
+});
+app.post('/users/login', (req,res) => {
+    var body = _.pick(req.body, ['email','password']);
+    User.findByCredentials(body.email,body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 
 //dev-routes
